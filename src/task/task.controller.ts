772cd4cc8a54f50c3b2cbbc552f9d6cfd8e89705
Ruther.f <robelfikadu70@gmail.com>
2025-07-13 13:47:37 +0300

    created routes on the controller

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,8 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 
 @Controller('tasks') // /task
 export class TaskController {
+    /* 
+    GET /tasks       CHECK
+    GET /tasks/:id   CHECK
+    POST /tasks      CHECK     
+    PATCH /tasks/:id CHECK
+    DELETE /tsks/:id CKECK
+    */
+
+
     
+    @Get()  // /task or /task?status=value 
+    getAllTasks(@Query('status') status?: 'PENDING' | 'IN-PROGRESS' | 'DONE') {
+        return []
+    }
+
+    @Get(':id')
+    getOneTask(@Param('id') id: string){
+        return { id }
+    }
+
+    @Post()
+    createTask(@Body() task: {}){
+        return task
+    }
+
+    @Patch(':id')
+    updateTask(@Param('id') id: string, @Body() taskUpdate: {}){
+        return {id, ...taskUpdate}
+    }
+
+    @Delete(':id')
+    deleteTask(@Param('id') id: string){
+        return []
+    }
+
 
 
 
